a92177387377a3d0a119284337e778e87d21737b
Po Lien Lin <polien.joe.lin@gmail.com>
2023-09-24 20:59:03 +0800

    Fix the path of picture

--- src/components/issues/IssueContainer.jsx
+++ src/components/issues/IssueContainer.jsx
@@ -27,12 +27,12 @@ const IssueContainer = () => {
           ?
             <Link className="button click-hide-a" to="/" onClick={actions.changeShowMore}>
               收起議題
-              <img className="arrow" alt="arrow" src="./aws_week4_assignment3/up-arrow.png" />
+              <img className="arrow" alt="arrow" src="https://imladris20.github.io/aws_week4_assignment3/up-arrow.png" />
             </Link>
           :
             <Link className="button click-hide-a" to="/" onClick={actions.changeShowMore}>
               看更多議題
-              <img className="arrow" alt="arrow" src="./aws_week4_assignment3/right-arrow.png" />
+              <img className="arrow" alt="arrow" src="https://imladris20.github.io/aws_week4_assignment3/up-arrow.png" />
             </Link>
         }
       </div>
